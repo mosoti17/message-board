07e9e8de5d68817ec3411059211f8df71050e1d1
mosoti17 <mosoti17@gmail.com>
2017-07-14 16:15:06 +0300

    added dynamic route

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contacts');
-  this.route('post', {path: '/post:post_id'});
+  this.route('post', {path: '/post/:post_id'});
 });
 
 export default Router;
